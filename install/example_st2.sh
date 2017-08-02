28e664c9da2ecca29b2c71cd0b2ff8a972270f24
ueno <ueno@nayuta.co>
2017-08-02 10:35:36 +0900

    fix comment

--- install/example_st2.sh
+++ install/example_st2.sh
@@ -14,7 +14,7 @@ cd ..
 
 
 # 0.01BTC(10mBTC)をP2WPKHアドレスに送金している。
-# fund.txt は、そのうち 9.9mBTC でチャネルを開き、5.0mBTCを相手に渡している。
+# fund.txt は、そのうち 9.0mBTC でチャネルを開き、5.0mBTCを相手に渡している。
 # なぜ 10mBTCで開かないかというと、P2WPKHアドレスからさらに funding_txに送金するため、
 # その分の FEEが必要になるからである(1mBTCもいらないとは思うが)。
 ./fund-in.sh 0.01 fund.txt > node_4444/fund4444_3333.conf
