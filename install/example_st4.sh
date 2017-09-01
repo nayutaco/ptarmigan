c1e571c69bfde78900b6a12dcc6f7849f2efc96d
ueno <ueno@nayuta.co>
2017-09-01 21:45:41 +0900

    Makefile依存関係

--- install/example_st4.sh
+++ install/example_st4.sh
@@ -28,7 +28,7 @@ fi
 #		3: (node_4444 ID),(node_4444--node_3333間short_channel_id),0.5mBTC(msat),CLTV
 #		4: (node_3333 ID),(node_3333--node_5555間short_channel_id),0.5mBTC(msat),CLTV
 #		5: (node_5555 ID),0,0.5mBTC(msat),CLTV
-# (最終行のmsatとCLTVは、その前と同じ値にしておく)。最終行のshort_channel_idは使っていない。
+#   ※実際にはFEEを含んだamountを使用すること
 echo `./ucoincli -c conf/peer3333.conf -i 50000000 5555` > pay4444_3333_5555.conf
 ./routing node_4444/dbucoin node_4444/node.conf `./ucoind ./node_5555/node.conf id` 50000000 >> pay4444_3333_5555.conf
 
