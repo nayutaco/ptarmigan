b7579a5d96d7ec7d20d9fc43c468629a281d567f
ueno <ueno@nayuta.co>
2017-12-11 20:50:56 +0900

    lib更新

--- update_libs.sh
+++ update_libs.sh
@@ -33,6 +33,7 @@ func_upd inih https://github.com/benhoyt/inih.git master
 cd ../ucoin/libs
 func_upd bech32 https://github.com/nayutaco/bech32.git master
 func_upd libbase58 https://github.com/luke-jr/libbase58.git master
+
 func_tag libsodium https://github.com/jedisct1/libsodium.git master 1.0.15
-func_upd lmdb https://github.com/LMDB/lmdb.git mdb.master
+func_tag lmdb https://github.com/LMDB/lmdb.git mdb.master LMDB_0.9.21
 func_tag mbedtls https://github.com/ARMmbed/mbedtls.git development mbedtls-2.6.1
