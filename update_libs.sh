b6caf8bf8ab9eaa966ccaa6b2ac8cb183202cb47
ueno <ueno@nayuta.co>
2017-12-12 10:45:06 +0900

    bech32 update

--- update_libs.sh
+++ update_libs.sh
@@ -31,7 +31,7 @@ func_upd jsonrpc-c https://github.com/nayutaco/jsonrpc-c.git localonly
 func_upd inih https://github.com/benhoyt/inih.git master
 
 cd ../ucoin/libs
-func_upd bech32 https://github.com/nayutaco/bech32.git master
+func_upd bech32 https://github.com/nayutaco/bech32.git ptarmigan
 func_upd libbase58 https://github.com/luke-jr/libbase58.git master
 
 func_tag libsodium https://github.com/jedisct1/libsodium.git master 1.0.15
