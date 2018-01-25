804cddc9d4699c8877d2c5be136d16adb6d68c3f
ueno <ueno@nayuta.co>
2018-01-25 15:22:08 +0900

    amount changes

--- install/script/established.sh
+++ install/script/established.sh
@@ -6,3 +6,6 @@
 #   $4: funding_txid
 DATE=`date +"%c %N"`
 echo { \"method\": \"established\", \"short_channel_id\": \"$1\", \"node_id\": \"$2\", \"date\": \"$DATE\", \"our_msat\": $3, \"debug\": \"funding_txid=$4\" } | jq .
+
+# our_msat
+echo $3 > our_msat.txt
--- install/script/htlcchanged.sh
+++ install/script/htlcchanged.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #   method: htlc_changed
 #   $1: short_channel_id
 #   $2: node_id
@@ -6,3 +6,15 @@
 #   $4: htlc_num
 DATE=`date +"%c %N"`
 echo { \"method\": \"htlc_changed\", \"short_channel_id\": \"$1\", \"node_id\": \"$2\", \"date\": \"$DATE\", \"our_msat\": $3, \"debug\": \"htlc_num=$4\" } | jq .
+
+# changes in amount
+if [ -f our_msat.txt ]; then
+    OUR_MSAT=`cat our_msat.txt`
+    pay=$(($3-$OUR_MSAT))
+    if [ $pay -gt 0 ]; then
+        echo GET: $pay msat!
+    fi
+    if [ $pay -lt 0 ]; then
+        echo PAY: $((0-$pay)) msat!
+    fi
+fi
