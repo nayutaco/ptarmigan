a97718c4c2d394aec1a99a24c2a1dddf9372f199
ueno <ueno@nayuta.co>
2018-02-22 11:37:43 +0900

    move tests scripts

--- tests/4nodes_test/example_st2.sh
+++ tests/4nodes_test/example_st2.sh
@@ -5,12 +5,16 @@
 # ここでは連続して起動させているが、動作を見る場合にはコンソールをそれぞれ開き、
 # 各コンソールで起動させた方がログを見やすい。
 cd node_3333
+rm -rf dbucoin
 ../ucoind -c ../regtest.conf -p 3333 &
 cd ../node_4444
+rm -rf dbucoin
 ../ucoind -c ../regtest.conf -p 4444 &
 cd ../node_5555
+rm -rf dbucoin
 ../ucoind -c ../regtest.conf -p 5555 &
 cd ../node_6666
+rm -rf dbucoin
 ../ucoind -c ../regtest.conf -p 6666 &
 cd ..
 
