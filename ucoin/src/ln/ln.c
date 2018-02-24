4faa1c54ca4a1e3b9199face42d376d7d24dcc80
ueno <ueno@nayuta.co>
2018-02-24 15:10:28 +0900

    shutdown受信待ちのときにping/pongを受付可能にする

--- ucoin/src/ln/ln.c
+++ ucoin/src/ln/ln.c
@@ -495,7 +495,9 @@ bool ln_recv(ln_self_t *self, const uint8_t *pData, uint16_t Len)
         DBG_PRINTF("fail: no init received : %04x\n", type);
         return false;
     }
-    if ((type != MSGTYPE_CLOSING_SIGNED) && !MSGTYPE_IS_ANNOUNCE(type) && ((self->shutdown_flag & M_SHDN_FLAG_END) == M_SHDN_FLAG_END)) {
+    if ( (type != MSGTYPE_CLOSING_SIGNED) &&
+         !MSGTYPE_IS_ANNOUNCE(type) && !MSGTYPE_IS_PINGPONG(type) &&
+         ((self->shutdown_flag & M_SHDN_FLAG_END) == M_SHDN_FLAG_END) ) {
         self->err = LNERR_INV_STATE;
         DBG_PRINTF("fail: not closing_signed received : %04x\n", type);
         return false;
