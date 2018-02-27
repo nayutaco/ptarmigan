e461082089d502859b2bb46222722ae6d8f3cd00
ueno <ueno@nayuta.co>
2018-02-27 19:31:55 +0900

    fix err script

--- install/script/error.sh
+++ install/script/error.sh
@@ -4,8 +4,8 @@
 #   $2: node_id
 #   $3: err_str
 DATE=`date +"%c %N"`
-echo { \"method\": \"error\", \"short_channel_id\": \"$1\", \"node_id\": \"$2\", \"date\": \"$DATE\", \"err_str\": \"$3\" } | jq .
+echo "{ \"method\": \"error\", \"short_channel_id\": \"$1\", \"node_id\": \"$2\", \"date\": \"$DATE\", \"err_str\": \"$3\" }" | jq .
 
-echo from $2($1): >> err_$1.log
-echo $3 >> err_$1.log
-echo >> err_$1.log
+echo "from $2($1):" >> err_$1.log
+echo "$3" >> err_$1.log
+echo "" >> err_$1.log
