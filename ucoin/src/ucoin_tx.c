5122e0c11a56918ec739913345ce70c775f799e3
ueno <ueno@nayuta.co>
2018-03-15 20:18:23 +0900

    HAVE TO REVOKE! for sign function check
    署名している関数を調べるためのcommit。
    後でrevokeすること。

--- ucoin/src/ucoin_tx.c
+++ ucoin/src/ucoin_tx.c
@@ -616,7 +616,7 @@ LABEL_EXIT:
 }
 
 
-bool ucoin_tx_sign(ucoin_buf_t *pSig, const uint8_t *pTxHash, const uint8_t *pPrivKey)
+bool ucoin_tx_sign_(ucoin_buf_t *pSig, const uint8_t *pTxHash, const uint8_t *pPrivKey)
 {
     int ret;
     bool bret;
@@ -660,7 +660,7 @@ LABEL_EXIT:
 }
 
 
-bool ucoin_tx_sign_rs(uint8_t *pRS, const uint8_t *pTxHash, const uint8_t *pPrivKey)
+bool ucoin_tx_sign_rs_(uint8_t *pRS, const uint8_t *pTxHash, const uint8_t *pPrivKey)
 {
     int ret;
     mbedtls_mpi r, s;
