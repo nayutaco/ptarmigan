d890c8955512700600ce3af122db40db252bca78
ueno <ueno@nayuta.co>
2018-04-15 22:10:51 +0900

    fix script json

--- install/script/fail.sh
+++ install/script/fail.sh
@@ -3,7 +3,7 @@
 #   $1: short_channel_id
 #   $2: node_id
 DATE=`date +"%Y-%m-%dT%H:%M:%S.%N"`
-echo { \"method\": \"fail\", \"short_channel_id\": $1, \"node_id\": \"$2\", \"date\": \"$DATE\" } | jq -c . > j.json
+echo { \"method\": \"fail\", \"short_channel_id\": \"$1\", \"node_id\": \"$2\", \"date\": \"$DATE\" } | jq -c . > j.json
 
 if [ -f script/PTARMTEST.txt ]; then
     PTARMTEST=`cat script/PTARMTEST.txt`
