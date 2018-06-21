05672205e22a85e00393318a1d0f534758e9dcb9
ueno <ueno@nayuta.co>
2018-06-21 19:53:32 +0900

    `-d`

--- tests/4nodes_test/example_st4fail4a.sh
+++ tests/4nodes_test/example_st4fail4a.sh
@@ -18,7 +18,7 @@ nodeid() {
 	cat conf/peer$1.conf | awk '(NR==3) { print $1 }' | cut -d '=' -f2
 }
 
-./routing -d $PAYER/dbucoin -s `nodeid $PAY_BEGIN` -r `nodeid $PAY_END` -a $AMOUNT
+./routing -d $PAYER -s `nodeid $PAY_BEGIN` -r `nodeid $PAY_END` -a $AMOUNT
 if [ $? -ne 0 ]; then
 	echo no routing
 	exit -1
@@ -31,7 +31,7 @@ if [ $? -ne 0 ]; then
 fi
 HASH=`echo $INVOICE | jq -r '.result.hash'`
 
-./routing -d $PAYER/dbucoin -s `nodeid $PAY_BEGIN` -r `nodeid $PAY_END` -a $AMOUNT > $ROUTECONF
+./routing -d $PAYER -s `nodeid $PAY_BEGIN` -r `nodeid $PAY_END` -a $AMOUNT > $ROUTECONF
 
 # fulfillしない
 ./ucoincli -d 1 $ACCIDENT_PORT
