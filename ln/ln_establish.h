0eb16af27e57a67a06145eb7d0d65f5ffe21363e
Hiroki Gondo <hiroki.gondo@nayuta.co>
2019-01-23 05:00:03 +0000

    ln/ln_establish: refactor

--- ln/ln_establish.h
+++ ln/ln_establish.h
@@ -43,15 +43,14 @@
 /** send open_channel
  *
  * @param[in,out]       self            channel info
- * @param[out]          pOpen           生成したopen_channelメッセージ
  * @param[in]           pFundin         fund-in情報
  * @param[in]           FundingSat      fundingするamount[satoshi]
  * @param[in]           PushSat         push_msatするamount[satoshi]
  * @param[in]           FeeRate         feerate_per_kw
  * retval       true    成功
  */
-bool /*HIDDEN*/ ln_open_channel_create(ln_self_t *self, utl_buf_t *pOpen,
-    const ln_fundin_t *pFundin, uint64_t FundingSat, uint64_t PushSat, uint32_t FeeRate);
+bool /*HIDDEN*/ ln_open_channel_send(
+    ln_self_t *self, const ln_fundin_t *pFundin, uint64_t FundingSat, uint64_t PushSat, uint32_t FeeRate);
 bool HIDDEN ln_open_channel_recv(ln_self_t *self, const uint8_t *pData, uint16_t Len);
 bool HIDDEN ln_accept_channel_send(ln_self_t *self);
 bool HIDDEN ln_accept_channel_recv(ln_self_t *self, const uint8_t *pData, uint16_t Len);
@@ -59,21 +58,9 @@ bool HIDDEN ln_funding_created_send(ln_self_t *self);
 bool HIDDEN ln_funding_created_recv(ln_self_t *self, const uint8_t *pData, uint16_t Len);
 bool HIDDEN ln_funding_signed_send(ln_self_t *self);
 bool HIDDEN ln_funding_signed_recv(ln_self_t *self, const uint8_t *pData, uint16_t Len);
-/**
- *
- * @param[in,out]       self            channel info
- * @param[out]          pLocked
- * @retval  true    成功
- */
-bool /*HIDDEN*/ ln_funding_locked_create(ln_self_t *self, utl_buf_t *pLocked);
+bool /*HIDDEN*/ ln_funding_locked_send(ln_self_t *self);
 bool HIDDEN ln_funding_locked_recv(ln_self_t *self, const uint8_t *pData, uint16_t Len);
-/** send channel_reestablish
- *
- * @param[in,out]       self            channel info
- * @param[out]          pReEst          channel_reestablishメッセージ
- * retval       true    成功
- */
-bool /*HIDDEN*/ ln_channel_reestablish_create(ln_self_t *self, utl_buf_t *pReEst);
+bool /*HIDDEN*/ ln_channel_reestablish_send(ln_self_t *self);
 bool HIDDEN ln_channel_reestablish_recv(ln_self_t *self, const uint8_t *pData, uint16_t Len);
 
 
