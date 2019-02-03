0ebafd9c174ce4a0b9aa119fa9382763e6332425
Hiroki Gondo <hiroki.gondo@nayuta.co>
2019-02-03 02:54:01 +0000

    tests/4nodes_test: `all_tests.sh` check logs

--- tests/4nodes_test/all_test.sh
+++ tests/4nodes_test/all_test.sh
@@ -86,10 +86,18 @@ PROC_COUNT=`ps -C ptarmd | grep ptarmd | wc -l`
 test $PROC_COUNT == 4
 echo check proc count end
 
+echo check log
+BAD_LINE_COUNT=`../../tools/log_filter.sh | wc -l`
+if [ $BAD_LINE_COUNT -ne 0 ];
+then
+    ../../tools/log_filter.sh
+    exit 1
+fi
+echo check log end
+
 echo clean start
 ./clean.sh
 echo clean end
-
 
 END=`date +%s`
 ELAPSED=`expr $END - $START` 
