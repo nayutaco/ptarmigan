509807048cddb53382873b2edad5299853e7dd2c
Hiroki Gondo <hiroki.gondo@nayuta.co>
2019-02-03 10:11:36 +0000

    tools: trivial

---
+++ tools/git_simple_backup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -ue
+
+#SCRIPT_NAME=$(basename $0)
+#SCRIPT_DIR=$(cd $(dirname $0); pwd)
+#PRJ_HOME=$SCRIPT_DIR/../
+
+(
+#cd $PRJ_HOME
+git branch backup || (git branch -D backup && git branch backup)
+#cd -
+)
