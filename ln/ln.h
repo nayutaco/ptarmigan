e333ed5b25c7db067fb43584e5a9ea23626d098b
Hiroki Gondo <hiroki.gondo@nayuta.co>
2019-02-09 03:34:15 +0000

    ln: add `ln_htlc.[ch]`

--- ln/ln.h
+++ ln/ln.h
@@ -45,6 +45,7 @@
 #include "ln_noise.h"
 #include "ln_node.h"
 #include "ln_script.h"
+#include "ln_htlc.h"
 
 
 #ifdef __cplusplus
@@ -86,17 +87,6 @@ extern "C" {
 #define LN_FUNDSAT_MIN                  (1000)      ///< minimum funding_sat(BOLTに規定はない)
 
 #define LN_FEE_COMMIT_BASE_WEIGHT       (724ULL)    ///< commit_tx base weight for the fee calculation
-
-// ln_htlc_flag_t.addhtlc
-#define LN_ADDHTLC_NONE                 (0x00)
-#define LN_ADDHTLC_SEND                 (0x01)      ///< Offered HTLC
-#define LN_ADDHTLC_RECV                 (0x02)      ///< Received HTLC
-
-// ln_htlc_flag_t.delhtlc, fin_delhtlc
-#define LN_DELHTLC_NONE                 (0x00)
-#define LN_DELHTLC_FULFILL              (0x01)      ///< update_fulfill_htlc/update_fail_htlc/update_fail_malformed_htlc送信済み
-#define LN_DELHTLC_FAIL                 (0x02)      ///< update_fail_htlc
-#define LN_DELHTLC_MALFORMED            (0x03)      ///< update_fail_malformed_htlc
 
 // channel_update.channel_flags
 #define LN_CNLUPD_CHFLAGS_DIRECTION     (0x01)      ///< b0: direction
@@ -353,44 +343,6 @@ typedef struct {
 /**************************************************************************
  * typedefs : Normal Operation
  **************************************************************************/
-
-/** @struct ln_htlc_flag_t
- *  @brief  HTLC管理フラグ
- *  @note
- *      - uint16_tとunionする場合がある
- */
-typedef struct {
-    unsigned        addhtlc     : 2;    ///< LN_ADDHTLC_SEND/RECV
-    unsigned        delhtlc     : 2;    ///< LN_DELHTLC_FULFILL/FAIL/MALFORMED
-    unsigned        updsend     : 1;    ///< 1:update message sent
-    unsigned        comsend     : 1;    ///< 1:commitment_signed sent
-    unsigned        revrecv     : 1;    ///< 1:revoke_and_ack received
-    unsigned        comrecv     : 1;    ///< 1:commitment_signed received
-    unsigned        revsend     : 1;    ///< 1:revoke_and_ack sent
-    unsigned        fin_delhtlc : 2;    ///< flag.addhtlc == RECV
-                                        //      update_add_htlc受信 && final node時、irrevocably committed後のflag.delhtlc
-    unsigned        Reserved    : 5;
-} ln_htlc_flag_t;
-
-#define LN_HTLCFLAG_MASK_HTLC       (0x000f)    ///< addhtlc, delhtlc
-#define LN_HTLCFLAG_MASK_UPDSEND    (0x0010)    ///< updsend
-#define LN_HTLCFLAG_MASK_COMSIG1    (0x0060)    ///< comsend, revrecv
-#define LN_HTLCFLAG_MASK_COMSIG2    (0x0180)    ///< comrecv, revsend
-#define LN_HTLCFLAG_MASK_COMSIG     ((LN_HTLCFLAG_MASK_COMSIG1 | LN_HTLCFLAG_MASK_COMSIG2))    ///< comsned, revrecv, comrecv, revsend
-#define LN_HTLCFLAG_MASK_FINDELHTLC (0x0600)    ///< fin_delhtlc
-#define LN_HTLCFLAG_MASK_UPDWAIT    (0x0800)    ///< updwait
-#define LN_HTLCFLAG_MASK_ALL        (LN_HTLCFLAG_MASK_FINDELHTLC | LN_HTLCFLAG_MASK_COMSIG | LN_HTLCFLAG_MASK_UPDSEND | LN_HTLCFLAG_MASK_HTLC)
-#define LN_HTLCFLAG_SFT_ADDHTLC(a)      ((uint16_t)(a))
-#define LN_HTLCFLAG_SFT_DELHTLC(a)      ((uint16_t)(a) << 2)
-#define LN_HTLCFLAG_SFT_UPDSEND         ((uint16_t)1 << 4)
-#define LN_HTLCFLAG_SFT_COMSEND         ((uint16_t)1 << 5)
-#define LN_HTLCFLAG_SFT_REVRECV         ((uint16_t)1 << 6)
-#define LN_HTLCFLAG_SFT_COMRECV         ((uint16_t)1 << 7)
-#define LN_HTLCFLAG_SFT_REVSEND         ((uint16_t)1 << 8)
-#define LN_HTLCFLAG_SFT_FINDELHTLC(a)   ((uint16_t)(a) << 9)
-#define LN_HTLCFLAG_SFT_UPDRECV         ((uint16_t)1 << 11)
-#define LN_HTLCFLAG_SFT_TIMEOUT         (LN_HTLCFLAG_SFT_REVSEND | LN_HTLCFLAG_SFT_COMRECV | LN_HTLCFLAG_SFT_REVRECV | LN_HTLCFLAG_SFT_COMSEND | LN_HTLCFLAG_SFT_UPDSEND | LN_HTLCFLAG_SFT_ADDHTLC(LN_ADDHTLC_SEND))
-
 
 /** @struct     ln_update_add_htlc_t
  *  @brief      update_add_htlc
