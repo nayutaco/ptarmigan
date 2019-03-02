060e48398869ae4d43e5d27f9e3f40a2e63643b6
Hiroki Gondo <hiroki.gondo@nayuta.co>
2019-03-02 23:39:30 +0000

    install/script: trivial

--- install/script/htlcchanged.sh
+++ install/script/htlcchanged.sh
@@ -3,8 +3,8 @@ set -eu
 #   method: htlc_changed
 #   $1: short_channel_id
 #   $2: node_id
-#   $3: our_msat
+#   $3: local_msat
 DATE=`date -u +"%Y-%m-%dT%H:%M:%S.%N"`
 cat << EOS | jq -e '.'
-{ "method":"htlc_changed", "short_channel_id":"$1", "node_id":"$2", "date":"$DATE", "our_msat":$3 }
+{ "method":"htlc_changed", "short_channel_id":"$1", "node_id":"$2", "date":"$DATE", "local_msat":$3 }
 EOS
