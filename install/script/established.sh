fe8cf8f2e6f1ffea9e95bd25b90aea0097c306a7
Hiroki Gondo <hiroki.gondo@nayuta.co>
2019-03-02 23:39:30 +0000

    install/script: trivial

--- install/script/established.sh
+++ install/script/established.sh
@@ -3,9 +3,9 @@ set -eu
 #   method: established
 #   $1: short_channel_id
 #   $2: node_id
-#   $3: our_msat
+#   $3: local_msat
 #   $4: funding_txid
 DATE=`date -u +"%Y-%m-%dT%H:%M:%S.%N"`
 cat << EOS | jq -e '.'
-{ "method":"established", "short_channel_id":"$1", "node_id":"$2", "date":"$DATE", "our_msat":$3, "funding_txid":"$4" }
+{ "method":"established", "short_channel_id":"$1", "node_id":"$2", "date":"$DATE", "local_msat":$3, "funding_txid":"$4" }
 EOS
