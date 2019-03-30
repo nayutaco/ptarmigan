a04302e52848cf37e971bf830eaa67ba7fbdd0da
ueno <ueno@nayuta.co>
2019-03-30 13:44:03 +0900

    tests: for bitcoinj

---
+++ tests/2nodes_test/example_st3j.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+#
+#
+#  +-----------+             +-----------+
+#  | node_4444 +-------------+ node_3333 |
+#  |    FUNDER |             |           |
+#  +-----------+             |    fundee |
+#                            +-----------+
+
+# connect
+./ptarmcli -c conf/peer3333.conf 4445
+
+
+ADDR=`./ptarmcli --getnewaddress 4445 | jq -er '.result'`
+echo sendtoaddress ${ADDR}
+bitcoin-cli -conf=`pwd`/regtest.conf -datadir=`pwd` sendtoaddress ${ADDR} 0.1
+bitcoin-cli -conf=`pwd`/regtest.conf -datadir=`pwd` generate 1
+sleep 3
+
+# node_4444からnode_3333へチャネルを開く。
+./ptarmcli -c conf/peer3333.conf -f 600000,300000 4445
+sleep 2
+
+# mining
+bitcoin-cli -conf=`pwd`/regtest.conf -datadir=`pwd` generate 6
+
+while :
+do
+    CHN3=`./showdb -c -d node_3333 | jq '.[]|length'`
+    CHN4=`./showdb -c -d node_4444 | jq '.[]|length'`
+    NOD3=`./showdb -n -d node_3333 | jq '.[]|length'`
+    NOD4=`./showdb -n -d node_4444 | jq '.[]|length'`
+    echo CHAN3=$CHN3:$NOD3 CHAN4=$CHN4:$NOD4
+
+    if [ "$CHN3" -gt 2 ] && [ "$CHN4" -gt 2 ] && [ "$NOD3" -eq 2 ] && [ "$NOD4" -eq 2 ]; then
+        break
+    fi
+    PTARMS3333=`./ptarmcli -l 3334 | grep -c total_local_msat`
+    PTARMS4444=`./ptarmcli -l 4445 | grep -c total_local_msat`
+    echo CLI3=${PTARMS3333} CLI4=${PTARMS4444}
+    if [ ${PTARMS3333} -eq 0 ] || [ ${PTARMS4444} -eq 0 ]; then
+        echo ptarmd exited
+        exit 1
+    fi
+    sleep 3
+done
+
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo @ channel established
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
