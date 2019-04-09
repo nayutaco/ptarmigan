3816bacdc58653ad6385d8008d0ee3cfb6960e43
Hiroki Gondo <hiroki.gondo@nayuta.co>
2019-04-09 06:04:56 +0000

    ln_db: add `ln_db_payment_invoice_*`

--- ln/ln_db.h
+++ ln/ln_db.h
@@ -878,6 +878,11 @@ bool ln_db_payment_route_save(uint64_t PaymentId, const uint8_t *pData, uint32_t
 bool ln_db_payment_route_load(utl_buf_t *pBuf, uint64_t PaymentId);
 bool ln_db_payment_route_del(uint64_t PaymentId);
 
+//XXX: comment
+bool ln_db_payment_invoice_save(uint64_t PaymentId, const uint8_t *pData, uint32_t Len);
+bool ln_db_payment_invoice_load(utl_buf_t *pBuf, uint64_t PaymentId);
+bool ln_db_payment_invoice_del(uint64_t PaymentId);
+
 
 /********************************************************************
  * others
