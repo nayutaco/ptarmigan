8df7853641d0fd9e7a10d7fcdb72f28cd6f2c784
Hiroki Gondo <hiroki.gondo@nayuta.co>
2019-04-16 23:00:47 +0000

    tools: update log_filter.sh

--- tests/4nodes_test/log_filter.sh
+++ tests/4nodes_test/log_filter.sh
@@ -4,7 +4,4 @@ SCRIPT_NAME=$(basename $0)
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 PRJ_HOME=$SCRIPT_DIR/../..
 
-for i in $SCRIPT_DIR/node_*/logs/log*
-do
-    $PRJ_HOME/tools/log_filter.sh $i
-done
+$PRJ_HOME/tools/log_filter.sh $SCRIPT_DIR/node_*/logs/log*
