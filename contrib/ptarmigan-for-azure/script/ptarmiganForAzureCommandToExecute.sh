25c4f0e589d94cb3edcac3586d772d744e37bb2e
YuNakashima <yu.nakashima@nayuta.co>
2019-04-19 14:55:07 +0900

    _artifactsLocation fix

--- contrib/ptarmigan-for-azure/script/ptarmiganForAzureCommandToExecute.sh
+++ contrib/ptarmigan-for-azure/script/ptarmiganForAzureCommandToExecute.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # bash ptarmiganForAzureCommandToExecute.sh "azureuser" "1" "18333" "18332" "9735" "false" ""
-echo "bash ptarmiganForAzureCommandToExecute.sh execute... ${1} ${2} ${3} ${4} ${5} ${6} ${7}"
+echo "step01 bash ptarmiganForAzureCommandToExecute.sh execute... ${1} ${2} ${3} ${4} ${5} ${6} ${7}"
 
 ADMIN_USER_NAME=${1:?}
 BITCOIN_ENVIRONMENT=${2:?}
@@ -18,22 +18,6 @@ BITCOIN_CONF_PATH=${HOME_PATH}/.bitcoin
 BITCOIND_PATH=/usr/local/bin
 BDB_PREFIX=${WORK_PATH}/bitcoin/db4
 
-
-echo "step01 package install"
-
-sudo apt -y update
-sudo apt -y upgrade
-
-sudo apt -y install build-essential
-sudo apt -y install libtool autotools-dev automake
-sudo apt -y install pkg-config bsdmainutils python3
-sudo apt -y install software-properties-common
-sudo apt -y install libboost-all-dev
-sudo apt -y install libdb4.8-dev libdb4.8++-dev
-sudo apt -y install libqrencode-dev autoconf openssl libssl-dev libevent-dev
-sudo apt -y install libminiupnpc-dev
-sudo apt -y install libzmq3-dev
-
 echo "step02 create configfile"
 
 # bitcoind
@@ -44,11 +28,11 @@ if [ ${BITCOIN_ENVIRONMENT} = "mainnet" ]; then
 elif [ ${BITCOIN_ENVIRONMENT} = "testnet" ]; then
   # testnet
   BITCOIN_ENVIRONMENT_SETTING="testnet=3 # testnet"
-  BITCOIN_PORT_SETTING="[testnet]\nport=${BITCOIN_PROTOCOL_PORT}"
+  BITCOIN_PORT_SETTING="[testnet]\nport=${BITCOIN_PROTOCOL_PORT}\nrpcport=${BITCOIN_RPC_PORT}"
 elif [ ${BITCOIN_ENVIRONMENT} = "regtest" ]; then
   # regtest
   BITCOIN_ENVIRONMENT_SETTING="regtest=1 # regtest"
-  BITCOIN_PORT_SETTING="[regtest]\nport=${BITCOIN_PROTOCOL_PORT}"
+  BITCOIN_PORT_SETTING="[regtest]\nport=${BITCOIN_PROTOCOL_PORT}\nrpcport=${BITCOIN_RPC_PORT}"
 fi
 
 mkdir -p $BITCOIN_CONF_PATH
@@ -56,13 +40,12 @@ cat << EOF > ${BITCOIN_CONF_PATH}/bitcoin.conf
 ${BITCOIN_ENVIRONMENT_SETTING}
 rpcuser=bitcoinuser
 rpcpassword=bitcoinpassword
-rpcport=${BITCOIN_RPC_PORT}
 rpcallowip=127.0.0.1
 listen=1
 server=1
 daemon=1
 txindex=1
-`echo ${BITCOIN_PORT_SETTING}`
+`echo -e ${BITCOIN_PORT_SETTING}`
 EOF
 
 chown -R ${ADMIN_USER_NAME}:${ADMIN_USER_NAME} $BITCOIN_CONF_PATH
@@ -105,8 +88,25 @@ StartLimitBurst=5
 WantedBy=multi-user.target
 EOF
 
+echo "step03 bitcoin install"
+mkdir -p ${BITCOIN_PATH}
+cd ${BITCOIN_PATH}
+wget https://bitcoincore.org/bin/bitcoin-core-0.17.1/bitcoin-0.17.1-x86_64-linux-gnu.tar.gz
+tar zxvf bitcoin-0.17.1-x86_64-linux-gnu.tar.gz
+sudo cp bitcoin-0.17.1/bin/* /usr/local/bin
 
-echo "step03 ptarmigan install"
+echo "step05 bitcoin service start"
+# bitcoind start
+systemctl daemon-reload
+sleep 3
+systemctl start bitcoin.service
+systemctl enable bitcoin.service >/dev/null 2>&1
+
+# bitcoind start and stop
+# systemctl start bitcoin.service
+# systemctl stop bitcoin.service
+
+echo "step04 ptarmigan install"
 sudo apt -y update
 sudo apt -y upgrade
 sudo apt install -y git autoconf pkg-config build-essential libtool wget jq bc
@@ -120,57 +120,30 @@ make full
 
 chown -R ${ADMIN_USER_NAME}:${ADMIN_USER_NAME} ${PTARMIGAN_PATH}
 
-echo "step04 bitcoin install"
-mkdir -p ${WORK_PATH}
-
-cd ${WORK_PATH}
-git clone https://github.com/bitcoin/bitcoin.git
-cd ${BITCOIN_PATH}
-./contrib/install_db4.sh `pwd`
-
-./autogen.sh
-./configure --without-gui BDB_LIBS="-L${BDB_PREFIX}/lib -ldb_cxx-4.8" BDB_CFLAGS="-I${BDB_PREFIX}/include"
-sudo make -j4
-sudo make install
-
-chown -R ${ADMIN_USER_NAME}:${ADMIN_USER_NAME} ${BITCOIN_PATH}
-
-echo "step05 bitcoin service start"
-
-# bitcoind start
-systemctl daemon-reload
-sleep 3
-systemctl start bitcoin.service
-systemctl enable bitcoin.service >/dev/null 2>&1
-
-# bitcoind start and stop
-# systemctl start bitcoin.service
-# systemctl stop bitcoin.service
-
-echo "step06 start ptarmign"
+echo "step05 start ptarmd"
 mkdir ${ADMIN_USER_NAME}:${ADMIN_USER_NAME} ${PTARMIGAN_PATH}/install/node
 chown -R ${ADMIN_USER_NAME}:${ADMIN_USER_NAME} ${PTARMIGAN_PATH}/install/node
 cd ${ADMIN_USER_NAME}:${ADMIN_USER_NAME} ${PTARMIGAN_PATH}/install/node
 
-# bitcoind
+# ptarmigan
 if [ ${BITCOIN_ENVIRONMENT} = "mainnet" ]; then
   # mainnet
-  ../ptarmd --network mainnet
+  nohup ../ptarmd --network mainnet --alias ${PTARMIGAN_NODE_ALIAS_NAME} >> ptarmd.log &
 elif [ ${BITCOIN_ENVIRONMENT} = "testnet" ]; then
   # testnet
-  ../ptarmd --network testnet
+  nohup ../ptarmd --network testnet --alias ${PTARMIGAN_NODE_ALIAS_NAME} >> ptarmd.log &
 elif [ ${BITCOIN_ENVIRONMENT} = "regtest" ]; then
   # regtest
-  ../ptarmd --network regtest
+  nohup ../ptarmd --network regtest --alias ${PTARMIGAN_NODE_ALIAS_NAME} >> ptarmd.log &
 fi
 
-echo "step07 install ptarmigan rest-api"
+echo "step06 install ptarmigan rest-api"
 cd ${WORK_PATH}
 git clone -b feature/rest-api https://github.com/nayutaco/ptarmigan.git ptarmigan-rest-api
 chown -R ${ADMIN_USER_NAME}:${ADMIN_USER_NAME} ${WORK_PATH}/ptarmigan-rest-api
 cd ${WORK_PATH}/ptarmigan-rest-api/ptarmapi
 sudo apt install -y npm
 npm install
-nohup npm run start &
+nohup npm run start >> ptarmapi.log &
 
-echo "step08 setup complete"
+echo "step07 install complete"
