4d6bad42234f22062ed1d9e2286e2ddda924ec6a
ueno <ueno@nayuta.co>
2019-06-10 14:51:57 +0900

    SPV mode use openjdk8

--- install/jdk.sh
+++ install/jdk.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 TARGET=x86_64
-#TARGET=RASPI
 #TARGET=RASPI_ARM11
 
 if [ "$TARGET" = "x86_64" ]; then
@@ -10,14 +9,8 @@ export JDK_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 export JDK_CPU=amd64/server
 echo $TARGET
 
-elif [ "$TARGET" = "RASPI" ]; then
-#JDK for Raspberry-Pi 2/3
-export JDK_HOME=/usr/lib/jvm/jdk-8-oracle-arm32-vfp-hflt
-export JDK_CPU=arm/server
-echo $TARGET
-
-elif [ "$TARGET" = "RASPI_ARM11" ]; then
-#JDK for Raspberry-Pi 1/Zero
+elif [ "$TARGET" = "ARM_RASPI" ]; then
+#JDK for openjdk-8-jdk (Raspberry-Pi)
 export JDK_HOME=/usr/lib/jvm/java-8-openjdk-armhf
 export JDK_CPU=arm/client
 echo $TARGET
