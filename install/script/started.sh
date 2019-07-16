ce24572e200e7401997c2142e053b319413da0f9
ueno <ueno@nayuta.co>
2019-07-16 21:02:53 +0900

    started.sh: local_msat

--- install/script/started.sh
+++ install/script/started.sh
@@ -3,12 +3,14 @@ set -eu
 #   method: started
 #   $1: short_channel_id
 #   $2: node_id
+#   $3: local_msat
 DATE=`date -u +"%Y-%m-%dT%H:%M:%S.%N"`
 cat << EOS | jq -e '.'
 {
     "method":"started",
     "date":"$DATE",
     "short_channel_id":"$1",
-    "node_id":"$2"
+    "node_id":"$2",
+    "local_msat":$3
 }
 EOS
