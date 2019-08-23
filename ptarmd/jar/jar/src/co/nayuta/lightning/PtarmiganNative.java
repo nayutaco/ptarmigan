0538b498416dcc4c9f8c784a2075d3cd7b075416
ueno <ueno@nayuta.co>
2019-08-23 18:25:15 +0900

    change name

--- ptarmd/jar/jar/src/co/nayuta/lightning/PtarmiganNative.java
+++ ptarmd/jar/jar/src/co/nayuta/lightning/PtarmiganNative.java
@@ -2,13 +2,12 @@ package co.nayuta.lightning;
 
 import javax.validation.constraints.NotNull;
 
-
 class PtarmiganNative {
     native static int ptarmStart(@NotNull String alias, @NotNull String ipAddress, int Port);
 
     public static void main(String args[]) {
         System.out.println("Hello, PtarmiganNative!");
-        int ret = ptarmStart("ueno", "", 3333);
+        int ret = ptarmStart("xxx", "", 3333);
         System.out.println("result=" + ret);
     }
 
