39e292a8eb92f94d15083a9c26384806efe4f02a
ueno <ueno@nayuta.co>
2021-10-19 09:57:20 +0900

    remove API

--- ptarmapi/script/set_addfinal.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-cp /ptarmigan/ptarmapi/script/addfinal.sh /ptarmigan/install/${NODE_NAME}/script
--- ptarmapi/src/app.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable, Logger } from '@nestjs/common';
-
-@Injectable()
-export class AppService {
-  getHello(): string {
-    return 'ptarmigan REST-API';
-  }
-}
